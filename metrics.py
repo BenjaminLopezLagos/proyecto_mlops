6a1c33d3842cc7727db6b7b6f5f8def99c11c39d
Benjamin <benjamin.lopezl@userena.cl>
2024-06-15 18:49:05 -0400

    updates to pipeline and future application with flask

--- metrics.py
+++ metrics.py
@@ -8,6 +8,10 @@ def main():
 
     model = YOLO(f'./models/model.pt')
     metrics = model.val()  # no arguments needed, dataset and settings remembered
+    metrics.box.map  # map50-95
+    metrics.box.map50  # map50
+    metrics.box.map75  # map75
+    metrics.box.maps  # a list contains map50-95 of each category
 
 if __name__ == '__main__':
     main()
